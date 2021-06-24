d035ba0f788836a0d09bae052a5e713bbdf2e785
Your Name <kuba.manda.km@gmail.com>
2021-06-24 13:25:37 +0100

    hackervoice

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,10 +2,13 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var password = req.query.password
-
+    var message = "Access denied"
+    if (password = "letmein"){
+        message = "Access granted"
+    }
    
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: message
     };
 }
