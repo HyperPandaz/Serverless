eae7b08ad24fba9605eaa880d53260030711f8c5
Your Name <kuba.manda.km@gmail.com>
2021-06-29 20:15:57 +0100

    update

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,25 +1,49 @@
 var multipart = require('parse-multipart');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     // here's your boundary:
     var boundary = multipart.getBoundary(req.headers['content-type']);
-
     // TODO: assign the body variable the correct value
-    var body = req.body
-
+    var body = req.body;
     // parse the body
     var parts = multipart.Parse(body, boundary);
 
-    var imageData = parts[0].data
-
-    var convertedResult = Buffer.from(imageData).toString('base64');
-// FILL IN THE BLANK
-
-
+    var imageData = parts[0].data;
+    //module.exports function
+    //analyze the image
+    var result = await analyzeImage(imageData);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: {
+            result
+        }
     };
+    console.log(result)
+    context.done();
+
+}
+
+async function analyzeImage(img) {
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnfaceAttributes': 'emotion'
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
+
+        //ADD YOUR TWO HEADERS HERE
+        headers: {
+            'Content-type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+
+    let data = await resp.json();
+    return data;
 }
