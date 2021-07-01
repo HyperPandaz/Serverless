fed4070bdf4a65e6fde787e426fead82910514fa
Your Name <kuba.manda.km@gmail.com>
2021-07-01 16:50:18 +0100

    favourite song

--- song4u/index.js
+++ song4u/index.js
@@ -7,6 +7,14 @@ module.exports = async function (context, req) {
 
     const queryObject = querystring.parse(req.body);
 
+    const songs = {
+        "GenZ": "https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf",
+        "GenY": "https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9",
+        "GenX": "https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d",
+        "BabyBoomers": "https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50",
+        "Unknown": "https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"
+    }
+
     let resp = await fetch(queryObject.MediaUrl0, {
         /*The await expression causes async function execution to pause until a Promise is settled 
         (that is, fulfilled or rejected), and to resume execution of the async function after fulfillment. 
@@ -21,23 +29,28 @@ module.exports = async function (context, req) {
 
     let age = result[0].faceAttributes.age;
 
-    if (age >= 5 && age <= 25){
+    if (age >= 5 && age < 25) {
         id = "GenZ"
     }
-    else if (age >= 26 && age <= 41){
+    else if (age >= 25 && age < 41) {
         id = "GenY"
     }
-    else if (age >= 42 && age <= 57){
+    else if (age >= 41 && age < 57) {
         id = "GenX"
     }
-    else if (age >= 58 && age <= 76){
+    else if (age >= 57 && age < 76) {
         id = "BabyBoomers"
     }
-    else {id = "Unknown"}
+    else { id = "Unknown" }
+
+    let value = songs[id];
+
+    let string = "We guessed you're part of this generation: ${id}! Happy listening! ${value}"
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: id
+        body: string
     };
 }
 
