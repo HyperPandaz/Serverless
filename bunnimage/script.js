6bd927532881aea6805cb9a52ef2feb615acb282
Your Name <kuba.manda.km@gmail.com>
2021-07-16 15:19:23 +0100

    imageupload

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,8 +1,28 @@
-function getImage(event){
+function getImage(event) {
     event.preventDefault();
-    if(document.getElementById("name").value.length == 0){
+    var bunniForm = document.getElementById("myform");
+    var payload = new FormData(bunniForm);
+    console.log(payload);
+    var fileInput = document.getElementById("image");
+    var fileName = document.getElementById("name");
+    const file = fileInput.files[0]; // fileInput is the file upload input element
+    payload.append("file", file);
+
+    if (document.getElementById("name").value.length != 0) {
+        try {
+            let url = "https://jakub-deployment.azurewebsites.net/api/bunnimage-upload?code=RWVI4br4W7PJHV0ushQLbeX19EtDZPhA7rNG/ruXmPXLXmBQDkp77g==";
+            console.log("Image was uploaded, making post request to Azure")
+            const response = await fetch(url, {
+                method: 'POST',
+                body: payload,
+                headers: { 'codename': fileName.value, }
+            });
+            $('#output').text("Your image has been stored successfully!");
+        } catch (e) {
+            $('#output').text(e);
+        }
+    } else {
         alert("No name error.");
-        return;
-    };
-    $('#output').text("Thanks!");
+    }
+
 }
