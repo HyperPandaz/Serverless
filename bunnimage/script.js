604dfe660d2baf51bd63c4e2dfd6e8cc7e4a779f
Your Name <kuba.manda.km@gmail.com>
2021-07-16 20:51:23 +0100

    download

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -8,21 +8,54 @@ function getImage(event) {
     const file = fileInput.files[0]; // fileInput is the file upload input element
     payload.append("file", file);
 
-    if (document.getElementById("name").value.length != 0) {
-        try {
-            let url = "https://jakub-deployment.azurewebsites.net/api/bunnimage-upload?code=RWVI4br4W7PJHV0ushQLbeX19EtDZPhA7rNG/ruXmPXLXmBQDkp77g==";
-            console.log("Image was uploaded, making post request to Azure")
-            const response = fetch(url, {
-                method: 'POST',
-                body: payload,
-                headers: { 'codename': fileName.value, }
-            });
-            $('#output').text("Your image has been stored successfully!");
-        } catch (e) {
-            $('#output').text(e);
-        }
-    } else {
+    if (fileName.value.length == 0) {
         alert("No name error.");
+        return;
+    }
+    if (file == null) {
+        alert("No file error");
+        return;
     }
 
+    try {
+        let url = "https://jakub-deployment.azurewebsites.net/api/bunnimage-upload?code=RWVI4br4W7PJHV0ushQLbeX19EtDZPhA7rNG/ruXmPXLXmBQDkp77g==";
+        console.log("Image was uploaded, making post request to Azure to upload")
+        const response = fetch(url, {
+            method: 'POST',
+            body: payload,
+            headers: { 'codename': fileName.value, }
+        });
+        $('#output').text("Your image has been stored successfully!");
+    } catch (e) {
+        $('#output').text(e);
+    }
+    return;
+}
+
+async function downloadImage(event) {
+    event.preventDefault();
+    var username = document.getElementById("downloadusername");
+    console.log(username.value);
+    if (username.value.length == 0) {
+        alert("No name error.");
+        return;
+    }
+    try {
+        let url = "https://jakub-deployment.azurewebsites.net/api/bunnimage-download?code=bw0CZ1shvZtU3MaKweP07BOfoTG5GXzIKCR7cg6aiHwJSWDXwRvrzA==";
+        console.log("Making get request to Azure to download")
+        const download = await fetch(url, {
+            method: 'GET',
+            headers: { 'username': username.value }
+        })
+
+        let data = await download.json();
+        let imageUrl = data.downloadUri;
+        console.log("Made get request successfuly");
+        console.log(data);
+        console.log(imageUrl);
+        window.open(imageUrl,"_self");
+    } catch (e) {
+        $('#output').text(e);
+    }
+    return;
 }
